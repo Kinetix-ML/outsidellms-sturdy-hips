38a49a639f0793fb7594d335556ab1066a53b482
Nader Khalil <nader@brev.dev>
2023-08-06 14:33:23 -0700

    another video

--- src/lib/Selector.svelte
+++ src/lib/Selector.svelte
@@ -1,16 +1,22 @@
 <script>
 	let blocks = [
-		{ title: 'HEART AWAY', artist: 'Artist 1', video: '/path/to/video1.mp4' },
-		{ title: 'DELI', artist: 'Artist 2', video: '/path/to/video2.mp4' },
-		{ title: 'CUFF IT', artist: 'Artist 3', video: '/path/to/video3.mp4' },
-		{ title: 'PUSHIN P', artist: 'Artist 4', video: '/path/to/video4.mp4' },
-		{ title: 'JERK', artist: 'Artist 5', video: '/path/to/video5.mp4' }
+		{ title: 'HEART AWAY', artist: 'Artist 1', video: 'src/lib/assets/heartaway.MP4' },
+		{ title: 'PRETTY BOY', artist: 'Artist 2', video: 'src/lib/assets/prettyboy.MP4' },
+		{ title: 'DELI', artist: 'Artist 2', video: 'src/lib/assets/prettyboy.MP4' },
+		{ title: 'CUFF IT', artist: 'Artist 3', video: 'src/lib/assets/prettyboy.MP4' },
+		{ title: 'PUSHIN P', artist: 'Artist 4', video: 'src/lib/assets/prettyboy.MP4' },
+		{ title: 'JERK', artist: 'Artist 5', video: 'src/lib/assets/prettyboy.MP4' }
 	];
 	let currBlock = 0;
 	let videoUrl = '';
 
 	function selectBlock(index) {
 		currBlock = index;
+		videoUrl = blocks[currBlock].video;
+	}
+
+	function backBlock() {
+		currBlock = (currBlock - 1) % blocks.length;
 		videoUrl = blocks[currBlock].video;
 	}
 
@@ -47,11 +53,14 @@
 </div>
 
 <div class="flex flex-row justify-between items-center text-black">
-	<button on:click={nextBlock} class="w-[200px] h-[100px] text-2xl bg-yellow-200 rounded-md">
-		Next
+	<button on:click={backBlock} class="w-[200px] h-[100px] text-2xl bg-yellow-200 rounded-md">
+		Back
 	</button>
 	<button on:click={goToDance} class="w-[200px] h-[100px] text-2xl bg-yellow-200 rounded-md">
 		Play
+	</button>
+	<button on:click={nextBlock} class="w-[200px] h-[100px] text-2xl bg-yellow-200 rounded-md">
+		Next
 	</button>
 </div>
 
